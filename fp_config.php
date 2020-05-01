3a5f5cdb0ad88804e33c4ea7c142c1cc6e46b4ab
RicKur <64633180+RicKur@users.noreply.github.com>
2020-05-01 23:15:01 +0700

    Add files via upload

--- fp_config.php
+++ fp_config.php
@@ -3,7 +3,7 @@ error_reporting(0);
 $config = array(
 	Coin		=>	"DOGE",	//Coin you Want Use Dice, Example BTC, DASH, LTC, DOGE, ETH, DGB, BCH
 	
-	Cookie		=>	"__cfduid=d2917c701967f1a6c7c7ee41763c1f2e31588062258; _ga=GA1.2.438094256.1588062260; session_token=1321e9077e7dd33dd13efa6e97dc69396acf4711b363944bcb90f221b82398fb; _gid=GA1.2.445500414.1588220048; cookieconsent_status=dismiss; cf_clearance=cd546c1cc845c24c7601ac3ddcb03f3f1060d810-1588345278-0-150; faucetpay=1lnj3q2nvvu8l3mtfs0v1krpk8; remember_me=215731%3A18dfc2b61ec5d70dc71a63abc48375eaca178def9eab2ab4cfcda4a7cd6ab056%3Ad97c6144a44fe032bfcdc101e47f568868f1aab1d3db736275a2235a417a3cfb; sc_is_visitor_unique=rx12149426.1588345339.335AAEE6BA114F1E67494F760D40F1E3.10.9.7.5.5.5.3.2.2",	//Your Cookie For Login Script
+	Cookie		=>	"Cookie: "__cfduid=d2917c701967f1a6c7c7ee41763c1f2e31588062258; _ga=GA1.2.438094256.1588062260; session_token=1321e9077e7dd33dd13efa6e97dc69396acf4711b363944bcb90f221b82398fb; _gid=GA1.2.445500414.1588220048; cookieconsent_status=dismiss; cf_clearance=489becfae46473720946d194f02accbab86f10ba-1588347812-0-150; remember_me=215731%3Ad3db0a34daab56d57d7dd6ad790aef8cc9806835a4e4de6430cffc29bf4859e0%3A3b6a530f24c088de991f93c304808f0b3daf2ae17d5c7e255b957bbd6bb1d790; faucetpay=7d7d29bf3p0clphu65nm0o57vo; _gat_gtag_UA_154633790_1=1; sc_is_visitor_unique=rx12149426.1588349571.335AAEE6BA114F1E67494F760D40F1E3.10.9.7.5.5.5.3.2.2",	//Your Cookie For Login Script
 	Name		=>	"FaucetPay Dice, by: RBS Terra",
 	Base_Bet	=>	"0.01",	//Base Bet For Frist Roll, Example 0.00000001
 	

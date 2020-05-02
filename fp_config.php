6a2f3d53e8eb0fdd771c49b6d9f9f7a5fdaadd2e
RicKur <64633180+RicKur@users.noreply.github.com>
2020-05-02 19:41:28 +0700

    Add files via upload

--- fp_config.php
+++ fp_config.php
@@ -3,9 +3,9 @@ error_reporting(0);
 $config = array(
 	Coin		=>	"DOGE",	//Coin you Want Use Dice, Example BTC, DASH, LTC, DOGE, ETH, DGB, BCH
 	
-	Cookie		=>	"Cookie: "__cfduid=d64c919923ed7c3dde6ab39712b0d42a11586978055;_ga=GA1.2.373608274.1586978056;cookieconsent_status=dismiss;source=40184;cf_clearance=6ec62262bf193f87117bd2e7801a454139e2c0da-1588338308-0-150;_gid=GA1.2.1842275688.1588338311;session_token=de900e1aaa16bdcfda8e63c01e54a7a2a0bac17ef53aa617a62878a8c1306f02;faucetpay=9opvq4o3l2edkmii3o6a5hdtef;remember_me=215731%3Adff32fccd67aeff1c286292123f5d134e4d0e2e75c884ac483e401b43c710468%3Aff0732d502484e668d3fe75a2974f5160059f6ba41baaa318f94e92a55253f49;_gat_gtag_UA_154633790_1=1;sc_is_visitor_unique=rx12149426.1588422411.6668C6ACAD6D4FB3202B31652F3BB932.16.10.10.8.6.4.3.3.2",	//Your Cookie For Login Script
+	Cookie		=>	"Cookie: "__cfduid=d2917c701967f1a6c7c7ee41763c1f2e31588062258; _ga=GA1.2.438094256.1588062260; _gid=GA1.2.445500414.1588220048; cookieconsent_status=dismiss; cf_clearance=489becfae46473720946d194f02accbab86f10ba-1588347812-0-150; faucetpay=12oq4ksjcqmc9e3mfgaqmgt2da; source=40184; remember_me=215731%3A8949f2167e7e909d25890f0e5e4f1282b9d43f081927bc8df11325b9a95e54de%3Ac019ea14179e5da20a0d89112b8559d99cb93f7729815c26907674580f3267fb; session_token=d109f2aa7e3480f5d3790f4fc7992d3397934fdab3ab92d08fd1c8aba5b7d0e4; sc_is_visitor_unique=rx12149426.1588423103.335AAEE6BA114F1E67494F760D40F1E3.14.13.9.7.6.6.4.3.3",	//Your Cookie For Login Script
 	Name		=>	"FaucetPay Dice, by: RBS Terra",
-	Base_Bet	=>	"0.01",	//Base Bet For Frist Roll, Example 0.00000001
+	Base_Bet	=>	"0.001",	//Base Bet For Frist Roll, Example 0.00000001
 	
 	//Your Win Chance Want use ,Example Min 1 and Max 94
 	Min_winChance	=>	"20", //Win Chance Minimum 1
@@ -20,7 +20,7 @@ $config = array(
 	
 	//Refresh to Base Bet if Profit
 	Refresh		=>	"0.001", //Refresh if Profit, Example 0.001
-	Stop_Profit =>	"1", //Stop If Profit to Target, Example 100.00
+	Stop_Profit =>	"100", //Stop If Profit to Target, Example 100.00
 	
 	//Reset Next if Win 
 	Roll_win	=>	"4"	//If Win Bet Change for How Much Roll, Example 1, 2, 3, 4, 5, etc.
